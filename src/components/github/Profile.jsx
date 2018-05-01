2af1cf7a92a99a134bd73cd1a6d386163bb4f840
Dariusz Franczak <darius32@op.pl>
2018-05-01 20:01:36 +0100

    all data rendered

--- src/components/github/Profile.jsx
+++ src/components/github/Profile.jsx
@@ -9,11 +9,11 @@ class Profile extends Component {
 				<div className="row">
 					<div className="col-3">
 	      		<img className="card-img-top" src={this.props.userData.avatar_url} alt="Card image cap"></img>
-						<h6 className="card-title">User: {this.props.userData.login}</h6>
+						<h6 className="card-title"></h6>
 					</div>
 					<div className="col-7">
 		        <div className="card-body">
-							<h5 className="card-title">Name: {this.props.userData.name}</h5>
+							<h5 className="card-title alert alert-primary">{this.props.userData.name}</h5>
 		    			<p className="card-text">
 								<span className="badge badge-info mr-1"> {this.props.userData.public_repos} Public Repos</span>
 								<span className="badge badge-secondary mr-1"> {this.props.userData.followers} Followers</span>
@@ -21,13 +21,12 @@ class Profile extends Component {
 							</p>
 		  			</div>
 					  <ul className="list-group list-group-flush">
-					    <li className="list-group-item">Cras justo odio</li>
-					    <li className="list-group-item">Dapibus ac facilisis in</li>
-					    <li className="list-group-item">Vestibulum at eros</li>
+					    <li className="list-group-item">Username: {this.props.userData.login}</li>
+					    <li className="list-group-item">Location: {this.props.userData.location}</li>
+					    <li className="list-group-item">Email: {this.props.userData.email}</li>
 					  </ul>
 					  <div className="card-body">
-					    <a href="#" className="card-link">Card link</a>
-					    <a href="#" className="card-link">Another link</a>
+					    <a href={this.props.userData.html_url} className="card-link btn btn-info" target="_blank">gitHub</a>
 					  </div>
 					</div>
 				</div>
